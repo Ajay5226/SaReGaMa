78cc27cddfe0e5e88244ea97b00762c9f8856c73
Ajay Shah <65993197+Ajay5226@users.noreply.github.com>
2021-06-20 18:40:11 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,20 @@
+let allMusic=[
+    {
+        name: "Vacation",
+        artist:"Dirty Heads",
+        img: "VacationPic",
+        src: "DirtyHeads-Vacation"
+    },
+    {
+        name: "Bilionera",
+        artist:"Otilia",
+        img: "BilioneraPic",
+        src: "Otilia-Bilionera"
+    },
+    {
+        name: "The Nights",
+        artist: "Avicii",
+        img: "TheNightsPic",
+        src: "Avicii-The Nights"      
+    }
+];
